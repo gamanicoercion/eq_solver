729a7a19ced794c7b8845cd1e935607367807632
gamanicoercion <140986172+gamanicoercion@users.noreply.github.com>
2023-12-23 22:03:54 +0200

    egg

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
-use std::io;
-use roots::Roots;
 use clearscreen;
+use roots::Roots;
+use std::io;
 
 enum EquationType {
     Linear,
     Quadratic,
     Cubic,
-    Quartic
+    Quartic,
 }
 
 fn vars(name: &str) -> f64 {
-    loop {let mut var = String::new();
+    loop {
+        let mut var = String::new();
         println!("Enter a value for {name}:");
-        io::stdin().read_line(&mut var).expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut var)
+            .expect("Failed to read line");
         let trimmed_var = var.trim();
         match trimmed_var.parse::<f64>() {
             Ok(i) => return i,
-            Err(..) => {println!("Not a viable input!"); continue}
+            Err(..) => {
+                println!("Not a viable input!");
+                continue;
+            }
         }
     }
 }
@@ -24,7 +30,7 @@ fn vars(name: &str) -> f64 {
 fn linear() -> f64 {
     let b = vars("b");
     let m = vars("m");
-    -b/m
+    -b / m
 }
 
 fn quad() -> Roots<f64> {
@@ -54,12 +60,32 @@ fn quartic() -> Roots<f64> {
 fn print_sols(sol: Roots<f64>) {
     let mut i = 1;
     match sol {
-        Roots::Four(sol) => for solution in sol {println!("Solution {}: {}", {i}, {solution}); i += 1},
-        Roots::Three(sol) => for solution in sol {println!("Solution {}: {}", {i}, {solution}); i += 1},
-        Roots::Two(sol) => for solution in sol {println!("Solution {}: {}", {i}, {solution}); i += 1},
-        Roots::One(sol) => for solution in sol {println!("Solution {}: {}", {i}, {solution}); i += 1},
-        _ => println!("No (Non-Complex) result.")
-        }   
+        Roots::Four(sol) => {
+            for solution in sol {
+                println!("Solution {}: {}", { i }, { solution });
+                i += 1
+            }
+        }
+        Roots::Three(sol) => {
+            for solution in sol {
+                println!("Solution {}: {}", { i }, { solution });
+                i += 1
+            }
+        }
+        Roots::Two(sol) => {
+            for solution in sol {
+                println!("Solution {}: {}", { i }, { solution });
+                i += 1
+            }
+        }
+        Roots::One(sol) => {
+            for solution in sol {
+                println!("Solution {}: {}", { i }, { solution });
+                i += 1
+            }
+        }
+        _ => println!("No (Non-Complex) result."),
+    }
 }
 
 fn main() {
@@ -69,24 +95,50 @@ fn main() {
         io::stdin()
             .read_line(&mut option)
             .expect("Failed to read line.");
-        match &*option.trim(){
-           "1" => {clearscreen::clear().expect("failed to clear screen");println!("Chosen: Linear Equation"); break EquationType::Linear},
-           "2" => {clearscreen::clear().expect("failed to clear screen"); println!("Chosen: Quadratic Equation"); break EquationType::Quadratic},
-           "3" => {clearscreen::clear().expect("failed to clear screen"); println!("Chosen: Cubic Equation"); break EquationType::Cubic},
-           "4" => {clearscreen::clear().expect("failed to clear screen"); println!("Chosen: Quartic Equation"); break EquationType::Quartic},
-           _ => {println!("{option} is not an option!");
-                continue}
+        match &*option.trim() {
+            "1" => {
+                clearscreen::clear().expect("failed to clear screen");
+                println!("Chosen: Linear Equation");
+                break EquationType::Linear;
+            }
+            "2" => {
+                clearscreen::clear().expect("failed to clear screen");
+                println!("Chosen: Quadratic Equation");
+                break EquationType::Quadratic;
+            }
+            "3" => {
+                clearscreen::clear().expect("failed to clear screen");
+                println!("Chosen: Cubic Equation");
+                break EquationType::Cubic;
+            }
+            "4" => {
+                clearscreen::clear().expect("failed to clear screen");
+                println!("Chosen: Quartic Equation");
+                break EquationType::Quartic;
+            }
+            _ => {
+                println!("{option} is not an option!");
+                continue;
+            }
         };
     };
-    
+
     match eq {
-        EquationType::Linear => { let sol = linear();
-            println!("Solution to linear equation is: {sol}")},
-        EquationType::Quadratic => {let sol = quad();
-            print_sols(sol) }
-        EquationType::Cubic => {let sol = cubic();
-            print_sols(sol);}
-        EquationType::Quartic => {let sol = quartic();
-            print_sols(sol);}
+        EquationType::Linear => {
+            let sol = linear();
+            println!("Solution to linear equation is: {sol}")
+        }
+        EquationType::Quadratic => {
+            let sol = quad();
+            print_sols(sol)
+        }
+        EquationType::Cubic => {
+            let sol = cubic();
+            print_sols(sol);
+        }
+        EquationType::Quartic => {
+            let sol = quartic();
+            print_sols(sol);
+        }
     }
 }
